c3082fc43087af32a4e3ece1e1e19e0487f280b8
dljimenez2324 <dljimenez2324@gmail.com>
2024-03-29 15:58:49 -0700

    added api get with string name input and outputs a personalized greeting that is random.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -36,7 +37,27 @@ app.UseHttpsRedirection();
 // .WithName("GetWeatherForecast")
 // .WithOpenApi();
 
-// 
+// Design an API endpoint that receives user input and outputs a personalized greeting, such as "Hello, {user's name}." 
+// when establishing parameters you can make them required by setting the variable without giving it an initial value, simply state the data type and variable name eg" string name"
+// if we use an "="  then the parameter will have a default value; however, its only good practice to have default values if its a very good reason.
+app.MapGet("/personalizedGreeting", (string name) => {
+    
+    // Array of strings with suggestions
+    string[] suggestion = {
+        "Go for a walk to be physically great!",
+        "Try making your own sourdough bread for gut-health greatness!",
+        "Take a nap!  You already work so hard! Get that mental & physical beauty greatness!",
+        "Call up a family member who would love to hear your voice!  Relationship greatness here you come!",
+        "Challenge yourself to learn something new every week to achieve greatness!",
+        "Embrace failure as a stepping stone towards achieving greatness!",
+        "Seek opportunities to step out of your comfort zone and embrace great growth!",
+        "Celebrate your achievements, no matter how small, to stay motivated on your journey towards greatness!"};
+    
+    // Index randomized obtained from random method and chosing from the 0 index (inclusive) to suggestion.Length (exclusive) indexes
+    int randomIndex = new Random().Next(0, suggestion.Length);
+    string greeting = "Hello " + name + "!  I hope you've achieved greatness today!  If not, maybe try some of these suggestions:  \n" + suggestion[randomIndex];
+    return greeting;
+});
 
 
 app.Run();
---
+++ README.md
@@ -0,0 +1,23 @@
+# Challenge - Weather App
+
+<b>Name:</b> David Jimenez<br>
+<b>Project:</b> Mini Challenge - 1 Endpoint <br>
+<b>Date:</b> 04/01/2024 <br>
+<strong>Description:</strong>
+Design an API endpoint that receives user input and outputs a personalized greeting, such as "Hello, {user's name}." 
+
+Utilize Postman to facilitate the data retrieval process.
+
+Things to turn in:
+- project, zipped
+- github link in comments
+
+<br><br>
+
+<b>Resources:</b> <br>
+<b>GitHub:</b> https://github.com/dljimenez2324/MiniChallenge_1_Endpoint <br>
+
+
+Peer Reviewed by _____________ <br>
+Comments:  <br> <br>
+
